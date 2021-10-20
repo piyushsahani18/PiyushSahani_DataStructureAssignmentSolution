b7d7ef8cdd1006b54648240dc0b3eb2c22d55659
piyushsahani18 <71111310+piyushsahani18@users.noreply.github.com>
2021-10-20 22:24:33 +0530

    Update Driver.java

--- GL-Assignment3_Que2/src/com/greatlearning/main/Driver.java
+++ GL-Assignment3_Que2/src/com/greatlearning/main/Driver.java
@@ -14,7 +14,7 @@ public class Driver {
 		tree.node.setLeft(new TransactionNode(30));
 		tree.node.setRight(new TransactionNode(60));
 		tree.node.getLeft().setLeft(new TransactionNode(10));
-		tree.node.getLeft().setRight(new TransactionNode(40));
+		tree.node.getRight().setLeft(new TransactionNode(55));
 		SkewTreeConversion skewTreeConversion = new SkewTreeConversion();
 		skewTreeConversion.BstToSkewTree(tree.node);
 		skewTreeConversion.traverseRightSkewed(skewTreeConversion.head);
