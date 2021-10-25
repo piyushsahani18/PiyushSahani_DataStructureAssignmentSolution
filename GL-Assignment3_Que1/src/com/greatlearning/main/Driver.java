a4537765a99d160979132a6ebd9aa286821ca36f
piyushsahani18 <71111310+piyushsahani18@users.noreply.github.com>
2021-10-25 09:17:27 +0530

    Update Driver.java

--- GL-Assignment3_Que1/src/com/greatlearning/main/Driver.java
+++ GL-Assignment3_Que1/src/com/greatlearning/main/Driver.java
@@ -1,21 +1,19 @@
 package com.greatlearning.main;
 
 import java.util.Scanner;
-
 import com.greatlearning.service.BuildingConstruction;
+import com.greatlearning.util.FloorException;
 
 public class Driver {
-	
-	public static void main(String[] args) {
+
+	static int totalFloors;
+	public static void main(String[] args) throws FloorException {
 		Scanner scanner = new Scanner(System.in);
 		BuildingConstruction construction = new BuildingConstruction();
 		System.out.println("Enter the total no of Floors in the building");
-        int totalFloors = scanner.nextInt();
-        int[] allfloors = construction.OrderOfFloorReceived(totalFloors);
-        construction.ConstructionOrder(allfloors, totalFloors);
-        scanner.close();
+		totalFloors = construction.getTotalFloors();
+		int[] allfloors = construction.OrderOfFloorReceived(totalFloors);
+		construction.ConstructionOrder(allfloors, totalFloors);
+		scanner.close();
 	}
-	
-	
-
 }
