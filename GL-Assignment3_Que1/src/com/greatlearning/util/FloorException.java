a35caf0054103dd614d5869bd9d8c245b38f7787
piyushsahani18 <71111310+piyushsahani18@users.noreply.github.com>
2021-10-25 09:26:39 +0530

    Add files via upload
    file to add new feature

---
+++ GL-Assignment3_Que1/src/com/greatlearning/util/FloorException.java
@@ -0,0 +1,17 @@
+package com.greatlearning.util;
+
+
+  @SuppressWarnings("serial")
+public class FloorException extends Exception {
+		
+	public FloorException() {
+		System.out.println("Floor size should be less than the building size");
+		
+	}
+	
+	
+	
+	
+	
+
+}
